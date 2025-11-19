2f874eae57a9ec0f234a914cc8a3033560caefc8
csef24-28 <mythicalphoenix067@gmail.com>
2025-11-19 10:42:05 +0530

    Update App.py

--- exp9/App.py
+++ exp9/App.py
@@ -1,26 +1,16 @@
-from flask import Flask, render_template, request
-import os
-import random
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-def random_image():
-    img_dir = "./static"
-    img_list = os.listdir(img_dir)
-    return random.choice(img_list)
+@app.route("/")
+def home(): return render_template("home.html")
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    image = random_image()
-    return render_template('home.html', home_image=image)
+@app.route("/about")
+def about(): return render_template("about.html")
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-    return render_template('registration.html')
+@app.route("/contact")
+def contact(): return render_template("contact.html")
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    return render_template('login.html')
+@app.route("/services")
+def services(): return render_template("services.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run()
