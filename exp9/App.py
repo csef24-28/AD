4dde908e3ee31d00e26aa13a30b7940318a499d4
csef24-28 <mythicalphoenix067@gmail.com>
2025-11-19 10:47:26 +0530

    Update App.py

--- exp9/App.py
+++ exp9/App.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home(): return render_template("home.html")
+def home():
+    return render_template("home.html")
 
 @app.route("/about")
-def about(): return render_template("about.html")
+def about():
+    return render_template("about.html")
 
 @app.route("/contact")
-def contact(): return render_template("contact.html")
-
-@app.route("/services")
-def services(): return render_template("services.html")
+def contact():
+    return render_template("contact.html")
 
 app.run()
